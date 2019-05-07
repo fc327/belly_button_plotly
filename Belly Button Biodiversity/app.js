5e542c5300604cff9ffb568becb12c11e645e2be
Farahin <38482100+fc327@users.noreply.github.com>
2019-05-07 00:46:50 -0400

    Update app.js

--- Belly Button Biodiversity/app.js
+++ Belly Button Biodiversity/app.js
@@ -1,14 +1,18 @@
-function buildMetadata(sample) {
+function buildMetadata(firstSample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-  var selector = Plotly.d3.select("#selDataset");
+  // var selector = d3.select("#selDataset");
 
-  d3.json("/names").then((sampleNames) => {
-      sampleNames.forEach((sample) => {
-          selector
-              .append("option")
-              .text(sample)
-              .property("value", sample);
+  d3.json("/metadata/"+firstSample).then((sampleNames) => {
+     console.log(sampleNames);
+     var panel=d3.select("#sample-metadata");
+     panel.html("");
+     var panelbody=Object.entries(sampleNames);
+    console.log(panelbody);
+    panelbody.forEach((metadatapair) => {
+      panel.append("h5").text(metadatapair[0]+":"+metadatapair[1]);
+      console.log(metadatapair);
+  });
       });
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
@@ -21,20 +25,21 @@ function buildMetadata(sample) {
 
 }
 
-function buildCharts(sample) {
+function buildCharts(firstSample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
 
     // @TODO: Build a Bubble Chart using the sample data
 
     // @TODO: Build a Pie Chart
-    function buildPie(sample) {
+    // function buildPie(irstSample) {
   console.log("This is a pie chart")
-  var url = `/samples/${sample}`
+  var url = `/samples/${firstSample}`
   d3.json(url).then((data) => {
-      var pieLabels = data.otu_ids.slice(0,10);
-      var pieValues = data.sample_values.slice(0,10);
-      var pieDescription = data.otu_labels.slice(0,10);
+      console.log(data);
+      const pieLabels = data.otu_ids
+      const pieValues = data.sample_values
+      const pieDescription = data.otu_labels
       console.log(pieLabels)
       console.log(pieValues)
       console.log(pieDescription)
@@ -44,8 +49,8 @@ function buildCharts(sample) {
       };
 
       var data = [{
-          values: pieValues,
-          labels: pieLabels,
+          values: pieValues.slice(0,10),
+          labels: pieLabels.slice(0,10),
           type: "pie",
           name: "Top 10 Samples",
           text_info: "percent",
@@ -54,12 +59,12 @@ function buildCharts(sample) {
           hoverinfo: "label+value+text+percent" 
       }];
 
-      Plotly.plot("pie", data, layout)
+      Plotly.plot("pie", data, layout);
   })
 }
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+
 
 function init() {
   // Grab a reference to the dropdown select element
@@ -67,6 +72,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("/names").then((sampleNames) => {
+    console.log(sampleNames);
     sampleNames.forEach((sample) => {
       selector
         .append("option")
@@ -76,15 +82,17 @@ function init() {
 
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
-    buildCharts(firstSample);
+    
     buildMetadata(firstSample);
+    buildCharts(firstSample);
   });
 }
 
-function optionChanged(newSample) {
+function optionChanged(firstSample) {
   // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+ 
+  buildMetadata(firstSample);
+  buildCharts(firstSample);
 }
 
 // Initialize the dashboard
